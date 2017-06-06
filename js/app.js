af6873e81f02c0fd710b80e21822ebe1962fffa1
zach hudson <zach.hudson@gmail.com>
2017-06-06 00:21:56 -0700

    cleanup javascript and update css for responsiveness

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-// Locations for the app. Objects.
+// Great Vegan restaurant locations in SF for the app
 var locations = [
 	{
 		"title" : "Shizen Vegan Sushi Bar",
@@ -40,7 +40,6 @@ var locations = [
 ];
 
 // Setting up for each individual location
-
 var Location = function(data) {
 	var self = this;
 	self.title = data.title,
@@ -59,17 +58,20 @@ var markers = [];
 // counter for the foursquare API calls on success and error
 var apiCallsCompleted = 0;
 
+
+// Call back function for the API. Keeps count of all API calls completed
+// Loads the app when finished just to make sure it grabs all ratings.
+// @index is the index from the ajax function
+// @rating is the rating returned from the Foursquare API
 function apiCallBack(index, rating) {
     apiCallsCompleted++;
-    console.log("total calls" + apiCallsCompleted);
-    console.log("rating" + rating);
     locations[index].rating = rating;
     if (apiCallsCompleted >= locations.length) {
         appLoader();
     }
 }
 
-
+// Calls out to foursquare api
 function fourSquareData(index) {
     var apiURL = 'https://api.foursquare.com/v2/venues/';
     var foursquareClientID = 'A2ZC3EKQFCXLXBEJPXQPA5YVVDG1EZYTW453HM4FY1HPOWS4'
@@ -80,11 +82,12 @@ function fourSquareData(index) {
     // URL using variables above
     var foursquareURL = apiURL + foursquareid + '?client_id=' + foursquareClientID +  '&client_secret=' + foursquareSecret +'&v=' + foursquareVersion;
 
+    // Reaches out to Foursquare and grabs the venue rating
+    // includes Graceful success and error handling of the Ajax call
     $.ajax({
         "url" : foursquareURL,
         "datatype" : "jsonp",
         "success" : function(response) {
-            console.log(response);
             resp = response.response.venue.rating;
             var rating = "Not yet rated"
             if (resp > 0) {
@@ -93,30 +96,35 @@ function fourSquareData(index) {
             apiCallBack(index, rating);
         },
         "error" : function(error) {
-            console.log('API error', foursquareid);
+            console.log('API error for Foursquareid ' + foursquareid);
             var rating = "Could not load results api error";
             apiCallBack(index, rating)
         }
     });
 };
 
+// Basic google maps constructor
 function initMap() {
-   // Map Constructor
-   map = new google.maps.Map(document.getElementById('map'), {
+    // Map Constructor
+    map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 37.785, lng: -122.446 },
     	zoom: 12,
     	styles: styles2,
     	mapTypeControl: false
     });
 
+    // runs through and gets the rating data
+    // bases it on the index.
     for (var i = 0; i < locations.length; i++){
         fourSquareData(i);
     }
 }
 
-
+// Continues loading the app
+// Runs only after the ratings are retrieved from the FS api
 function appLoader() {
 
+    // info window provides from google maps
     var largeInfoWindow = new google.maps.InfoWindow();
 
     // style the markers
@@ -125,8 +133,12 @@ function appLoader() {
     // create a "highlighted location" marker color
     var highlightedIcon = makeMarkerIcon('FFFF24');
 
+    // sets bounds on load to zoom in on San Francisco
     var bounds = new google.maps.LatLngBounds();
 
+    // Loops through all locations and assigns
+    // a marker to each of them.
+    // this also attaches the FS venue rating within the title.
     for (var i = 0; i < locations.length; i++) {
         var rating = locations[i].rating;
         var position = locations[i].location;
@@ -147,39 +159,50 @@ function appLoader() {
         locations[i].marker.addListener('click', function() {
             populateInfoWindow(this, largeInfoWindow);
         });
+
         // two event listeners - one for mouse over, one ofr mouseout,
         // to change the colors back and forth
         locations[i].marker.addListener('mouseover', function() {
             this.setIcon(highlightedIcon);
         });
+
+        // Changes the marker once you mouse off.
         locations[i].marker.addListener('mouseout', function() {
             this.setIcon(defaultIcon);
         });
+
+        // sets the map
+        // extends the bounds to include the markers.
         markers[i].setMap(map);
         bounds.extend(locations[i].marker.position);
     }
 
+    // sets map and fits bounds.
     map.fitBounds(bounds);
 
-    // Show pins when clicked
+    // Show pins when clicked (just a nice user function)
+    // TODO: Make search function work with this.
     document.getElementById('show-listings').addEventListener('click', showListings);
 
-    // Hide all pins when clicked
+    // Hide all pins when clicked (just a nice user function)
+    // TODO: make search function integrate with this.
     document.getElementById('hide-listings').addEventListener('click', function() {
         hideMarkers(markers);
     });
 };
 
-
+// Errors out for Google maps according to their documentation
 function mapError() {
     alert("Sorry!  Google Maps could not be loaded");
 }
 
+// populates the boxes that pop up when a marker is clicked.
 function populateInfoWindow(marker, infowindow) {
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
         infowindow.setContent('<div>' + marker.title + '</div>');
         infowindow.open(map, marker);
+
         // Making sure the marker property is cleared if the infowindow is closed.
         infowindow.addListener('closeclick',function(){
             infowindow.setMap(null);
@@ -190,7 +213,7 @@ function populateInfoWindow(marker, infowindow) {
 
         // In case the status is OK, which means the pano was found, compute the
         // position of the streetview image, then calculate the heading, then get a
-        // panorama from that and set the options
+        // panorama from that and set the options.
         function getStreetView(data, status) {
             if (status == google.maps.StreetViewStatus.OK) {
                 var nearStreetViewLocation = data.location.latLng;
@@ -212,7 +235,7 @@ function populateInfoWindow(marker, infowindow) {
             }
         };
         // Use streetview service to get the closest streetview image within
-        // 50 meters of the markers position
+        // 50 meters of the markers position.
         streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
         // Open the infowindow on the correct marker.
         infowindow.open(map, marker);
@@ -237,6 +260,7 @@ function hideMarkers(markers) {
     }
 }
 
+// sets the marker icon
 function makeMarkerIcon(markerColor) {
     var markerImage = new google.maps.MarkerImage(
         'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
@@ -248,7 +272,9 @@ function makeMarkerIcon(markerColor) {
     return markerImage;
 }
 
-
+// Knockout JS viewmodel function
+// listSelect: handles when location is selected from list on the left hand side.
+// search entry: handles the search functionality of the viewmodel.
 var AppViewModel = function() {
     var self = this;
     self.appLocations = ko.observableArray(locations.slice());
@@ -284,6 +310,8 @@ var appViewModel = new AppViewModel();
 // activate knockout and apply binding.
 ko.applyBindings(appViewModel);
 
+// all the styles for the google maps.
+// nice and green and healthy looking!
 var styles2 = [
     {
         "featureType": "landscape",
